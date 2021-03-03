13a27b2fe0654146fb8faf8d395c688882de9443
Matthew Dupont <matthewdupont11@gmail.com>
2021-03-03 17:06:32 -0600

    Adding Reduce implementation to roundRobin; adding a .gitignore

--- roundRobinPolynomial.c
+++ roundRobinPolynomial.c
@@ -1,6 +1,6 @@
 /*
   COSC 6060
-  Matthew Dupont
+  Matthew Dupont and Louis Finney
   Assignment 2 - MPI work distribution
 */
 
@@ -23,8 +23,7 @@ int main(int argc, char** argv) {
   int i;
   printf("Initializing polynomials\n");
   int *coeffArr = (int *)malloc(sizeof(int) * NUM_POLYNOMIALS);
-  initialize(coeffArr, NUM_POLYNOMIALS); 
-  
+  initialize(coeffArr, NUM_POLYNOMIALS);
   
   /* Start timer */
   double elapsed_time = - MPI_Wtime();
@@ -32,25 +31,32 @@ int main(int argc, char** argv) {
   //Processes calculate work as local totals using round robin.
   double localTotal = 0.0;
   for (i = rank; i < NUM_POLYNOMIALS; i += numProcs) {
-    //printf("LocalTotal:%f\tCoef:%i\tDegree:%i\tVariable:%f\n", localTotal, coeffArr[i], i, VARIABLE);
+    //DEBUG printf("LocalTotal:%f\tCoef:%i\tDegree:%i\tVariable:%f\n", localTotal, coeffArr[i], i, VARIABLE);
     localTotal += evaluateTerm(coeffArr[i], i, VARIABLE);
     printf("Thread %i Evaluated Term %i, new local total %f\n", rank, i, localTotal);
   }
   printf("Thread %i\tCompleted - Total %f\n", rank, localTotal);
-
-
-  //Aggregate results with message passing
-  double result;
-  MPI_Status status;
-  if(rank == 0){
-    for (i = 1; i<numProcs; i++) {
-      MPI_Recv(&result, 1, MPI_DOUBLE, i, 1, MPI_COMM_WORLD, &status);
-      localTotal += result;
-    }
-    printf("\n\nGlobal Total: %f\n", localTotal);
-  } else {
-    MPI_Send(&localTotal, 1, MPI_DOUBLE, 0, 1, MPI_COMM_WORLD);
+  
+  
+  //Aggregate results with reduce
+  double result = 0.0;
+  MPI_Reduce(&localTotal, &result, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD); 
+  if (rank == 0) {
+    printf("\n\nGlobal Total: %f\n", result);
   }
+
+  // //Aggregate results with message passing
+  // double result;
+  // MPI_Status status;
+  // if(rank == 0){
+  //   for (i = 1; i<numProcs; i++) {
+  //     MPI_Recv(&result, 1, MPI_DOUBLE, i, 1, MPI_COMM_WORLD, &status);
+  //     localTotal += result;
+  //   }
+  //   printf("\n\nGlobal Total: %f\n", localTotal);
+  // } else {
+  //   MPI_Send(&localTotal, 1, MPI_DOUBLE, 0, 1, MPI_COMM_WORLD);
+  // }
  
   /* End timer */
   elapsed_time = elapsed_time + MPI_Wtime();
