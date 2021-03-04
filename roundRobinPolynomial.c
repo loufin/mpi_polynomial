21d0b7712ee8283725691473b11000c992716961
Matthew Dupont <matthewdupont11@gmail.com>
2021-03-04 12:09:34 -0600

    splitting out some functions in roundRobinPolynomial

--- roundRobinPolynomial.c
+++ roundRobinPolynomial.c
@@ -9,55 +9,69 @@
 #include <stdlib.h>
 #include "polynomial.h"
 
-#define NUM_POLYNOMIALS 50000
+double calculateTotal(int* polynomials, int numPolynomials, int rank, int numProcs, double variable) {
+  double localTotal = 0.0;
+  int i;
+  for (i = rank; i < numPolynomials; i += numProcs) {
+    //DEBUG printf("LocalTotal:%f\tCoef:%i\tDegree:%i\tVariable:%f\n", localTotal, coeffArr[i], i, VARIABLE);
+    localTotal += evaluateTerm(polynomials[i], i, variable);
+    //DEBUG printf("Thread %i Evaluated Term %i, new local total %f\n", rank, i, localTotal);
+  }
+  printf("Thread %i\tCompleted - Total %f\n", rank, localTotal);
+  return localTotal;
+}
+
+double aggregate_message_passing(double localTotal, int rank, int numProcs) {
+  //Aggregate results with message passing
+  double result;
+  int i;
+  MPI_Status status;
+  
+  if(rank == 0){
+    for (i = 1; i<numProcs; i++) {
+      MPI_Recv(&result, 1, MPI_DOUBLE, i, 1, MPI_COMM_WORLD, &status);
+      localTotal += result;
+    }
+    return localTotal;
+  } else {
+    MPI_Send(&localTotal, 1, MPI_DOUBLE, 0, 1, MPI_COMM_WORLD);
+    return 0.0;
+  }
+}
 
 int main(int argc, char** argv) {
   int rank;
   int numProcs;
-	
+
+  int numPolynomials = 50000;
+  if (argc == 2) {
+    numPolynomials = atoi(argv[1]);
+  }
+
   MPI_Init(&argc, &argv);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &numProcs);
 
   //Each process generates the full set of coefficients.
-  int i;
   printf("Initializing polynomials\n");
-  int *coeffArr = (int *)malloc(sizeof(int) * NUM_POLYNOMIALS);
-  initialize(coeffArr, NUM_POLYNOMIALS);
+  int *coeffArr = (int *)malloc(sizeof(int) * numPolynomials);
+  initialize(coeffArr, numPolynomials);
   
   /* Start timer */
   double elapsed_time = - MPI_Wtime();
     
   //Processes calculate work as local totals using round robin.
-  double localTotal = 0.0;
-  for (i = rank; i < NUM_POLYNOMIALS; i += numProcs) {
-    //DEBUG printf("LocalTotal:%f\tCoef:%i\tDegree:%i\tVariable:%f\n", localTotal, coeffArr[i], i, VARIABLE);
-    localTotal += evaluateTerm(coeffArr[i], i, VARIABLE);
-    printf("Thread %i Evaluated Term %i, new local total %f\n", rank, i, localTotal);
-  }
-  printf("Thread %i\tCompleted - Total %f\n", rank, localTotal);
-  
+  double localTotal = calculateTotal(coeffArr, numPolynomials, rank, numProcs, VARIABLE);
   
   //Aggregate results with reduce
   double result = 0.0;
-  MPI_Reduce(&localTotal, &result, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD); 
+  //MPI_Reduce(&localTotal, &result, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD); 
+  result = aggregate_message_passing(localTotal, rank, numProcs);
+
   if (rank == 0) {
     printf("\n\nGlobal Total: %f\n", result);
   }
 
-  // //Aggregate results with message passing
-  // double result;
-  // MPI_Status status;
-  // if(rank == 0){
-  //   for (i = 1; i<numProcs; i++) {
-  //     MPI_Recv(&result, 1, MPI_DOUBLE, i, 1, MPI_COMM_WORLD, &status);
-  //     localTotal += result;
-  //   }
-  //   printf("\n\nGlobal Total: %f\n", localTotal);
-  // } else {
-  //   MPI_Send(&localTotal, 1, MPI_DOUBLE, 0, 1, MPI_COMM_WORLD);
-  // }
- 
   /* End timer */
   elapsed_time = elapsed_time + MPI_Wtime();
   printf(" sequential value %f wall clock time %8.6f \n", localTotal, elapsed_time);  
